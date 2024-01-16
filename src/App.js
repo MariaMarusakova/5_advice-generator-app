9f61d42a58e3a42276c8dece2bb246fc32a3a27e
MariaMarusakova <maruska.marusakova@gmail.com>
2024-01-16 20:12:42 +0100

    add async / await

--- src/App.js
+++ src/App.js
@@ -5,26 +5,37 @@ import React, { useState, useEffect } from "react";
 function App() {
 
   const [advice, setAdvice] = useState(null);
-  useEffect(() => {
-    fetch('https://api.adviceslip.com/advice')
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
 
-        setAdvice(data.slip.advice);
-      })
-      .catch(error => console.error(error));
-    }, []);
+  const fetchAdvice = async () => {
+    try {
 
-    return (
-      <div className="App">
+      const response = await fetch('https://api.adviceslip.com/advice');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-        <div>
-          <h2>Advice of the day:</h2>
-          {advice && <p>{advice}</p>}
-        </div>
-      </div>
-    );
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+
   }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div className="App">
+
+      <div>
+        <h2>Advice of the day:</h2>
+        {advice && <p>{advice}</p>}
+      </div>
+    </div>
+  );
+}
 
 export default App;
