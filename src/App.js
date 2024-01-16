6918eedb2b6dca19d6ad3fe42c82884de455dcb8
MariaMarusakova <maruska.marusakova@gmail.com>
2024-01-16 20:06:14 +0100

    fetch from API and display an advice (no style zet)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [advice, setAdvice] = useState(null);
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+
+        setAdvice(data.slip.advice);
+      })
+      .catch(error => console.error(error));
+    }, []);
+
+    return (
+      <div className="App">
+
+        <div>
+          <h2>Advice of the day:</h2>
+          {advice && <p>{advice}</p>}
+        </div>
+      </div>
+    );
+  }
 
 export default App;
