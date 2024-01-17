fbef2523631545f98017068a22513df543e17bb3
MariaMarusakova <maruska.marusakova@gmail.com>
2024-01-17 21:01:14 +0100

    acceptable styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { ReactComponent as Dice } from './images/icon-dice.svg';
 import { ReactComponent as Divider } from './images/pattern-divider-desktop.svg';
 
@@ -31,15 +30,19 @@ function App() {
   }, []);
 
   return (
-    <div className="main-container">
-      <div className="advice-container">
-        {advice && <h2>Advice #{advice.id}</h2>}
-        {advice && <div className='advice'>{advice.advice}</div>}
-        <div><Divider /></div>
-        <div><Dice /></div>
+    <body>
+      <div className="main-container">
+        <div className="advice-container">
+          {advice && <h2>Advice #{advice.id}</h2>}
+          {advice && <div className='advice'>"{advice.advice}"</div>}
+          <div className='bottom-part'>
+            <div className='divider'><Divider /></div>
+            <button><Dice /></button>
+          </div>
 
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
